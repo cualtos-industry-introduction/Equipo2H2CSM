487ea2e2d762eec427898be516e4dbdd611e2235
horaciogomezmx <horaciogomezmx@gmail.com>
2020-07-03 18:57:28 -0500

    creacionmenu

---
+++ menu.py
@@ -0,0 +1,16 @@
+from agenda import Agenda
+from contacto import Contacto
+
+agenda = Agenda('nueva_agenda')
+contactos = agenda.obtenerContactos()
+
+def mostrar_menu():
+    print("Agenda")
+    print("Selecciona la acción que desees efectuar")
+    print("1. Agregar contactos")
+    print("2. Mostar contactos")
+    print("3. Consulta de contactos")
+    print("4. Actualizar contactos")
+    print("3. Eliminar contactos")
+    print("9. Salir")
+    
