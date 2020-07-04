999c11f7a3111e19902ebbf48b352af9646a4788
horaciogomezmx <horaciogomezmx@gmail.com>
2020-07-03 19:09:52 -0500

    mascambios

--- menu.py
+++ menu.py
@@ -4,6 +4,10 @@ from contacto import Contacto
 agenda = Agenda('nueva_agenda')
 contactos = agenda.obtenerContactos()
 
+
+    
+    while(salida == "1"):
+
 def mostrar_menu():
     print("Agenda")
     print("Selecciona la acción que desees efectuar")
@@ -13,4 +17,21 @@ def mostrar_menu():
     print("4. Actualizar contactos")
     print("3. Eliminar contactos")
     print("9. Salir")
+
+    entrada = input ("Escribe la opcion: ")
     
+    if entrada == "Agregar":
+        agregarContacto()
+        print("Agregar contactos")
+    elif entrada == "Mostrar":
+        print(lista[])
+    elif entrada == "Consulta":
+        print(lista[])   
+    elif entrada == "Actualizar":
+        print(lista[])     
+    elif entrada == "Eliminar":
+        print(lista[])
+    elif entrada ==  "Salir":
+        exit()
+    else:
+        print("Opcoin no valida")
